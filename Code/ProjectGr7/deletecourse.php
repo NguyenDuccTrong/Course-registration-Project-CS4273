a09d99aecb31ce77550a6b7c1c9e42473e1aeae6
ThanhOwO <91793770+ThanhOwO@users.noreply.github.com>
2021-12-05 17:52:27 +0700

    Delete Code/ProjectGr7 directory

--- Code/ProjectGr7/deletecourse.php
+++
@@ -1,9 +0,0 @@
-<?php
-    $conn = mysqli_connect('localhost','root','','registerphase');
-    $Mamonhoc = $_GET['Mamonhoc'];
-    $sql = "DELETE FROM danhsachdadangky WHERE Mamonhoc = '$Mamonhoc'";
-    $kq = mysqli_query($conn, $sql);
-    if($kq){
-        header("location:editcourse.php");
-    }
-?>
